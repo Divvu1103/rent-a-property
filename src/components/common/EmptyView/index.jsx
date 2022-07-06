5d8c75737f1433cf16133cf6eae6f11bd2db671e
Divvu1103 <200110075@hbtu.ac.in>
2022-07-07 01:20:38 +0530

    created

---
+++ src/components/common/EmptyView/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const EmptyView = () => (
+  <div className='emptyView-wrap'>
+    <p>no result available</p>
+  </div>
+);
+
+export default EmptyView;
---
+++ src/constants/index.js
@@ -0,0 +1,170 @@
+export const categoryList = [
+  {
+    id: 1,
+    value: "place",
+    label: " Places",
+  },
+  {
+    id: 2,
+    value: "hotel",
+    label: " hotels",
+  },
+];
+
+export const ratingList = [
+  {
+    id: 1,
+    value: "1",
+    label: "1🌟",
+  },
+  {
+    id: 2,
+    value: "2",
+    label: "2🌟",
+  },
+  {
+    id: 3,
+    value: "3",
+    label: "3🌟",
+  },
+  {
+    id: 4,
+    value: "4",
+    label: "4🌟",
+  },
+  {
+    id: 5,
+    value: "5",
+    label: "5🌟",
+  },
+];
+
+export const dataList = [
+  {
+    id: 1,
+    title: "Palm Harbor",
+
+    category: "place",
+    cuisine: "urban",
+    rating: 5,
+    price: 2500,
+    coverSrc:
+      "https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 2,
+    title: "Beverly Springfield",
+
+    category: "place",
+    cuisine: "city",
+    rating: 4,
+    price: 1000,
+    coverSrc:
+      "https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 3,
+    title: "Faulkner Avenue",
+
+    category: "place",
+    cuisine: "village",
+    rating: 3,
+    price: 2000,
+    coverSrc:
+      "https://images.unsplash.com/photo-1523217582562-09d0def993a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 4,
+    title: "Franklin",
+
+    category: "place",
+    cuisine: "urban",
+    rating: 1,
+    price: 1800,
+    coverSrc:
+      "https://images.unsplash.com/photo-1593604340846-4fbe9763a8f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 5,
+    title: "Clinton",
+
+    category: "place",
+    cuisine: "city",
+    rating: 5,
+    price: 2000,
+    coverSrc:
+      "https://images.unsplash.com/photo-1576941089067-2de3c901e126?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 6,
+    title: "Lebanon",
+
+    category: "place",
+    cuisine: "village",
+    rating: 5,
+    price: 3500,
+    coverSrc:
+      "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 7,
+    title: "Crowne Plaza",
+    category: "hotel",
+    cuisine: "urban",
+    rating: 2,
+    price: 2200,
+    coverSrc:
+      "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aG90ZWxzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 8,
+    title: "Emerald Bay Inn",
+    category: "hotel",
+    cuisine: "city",
+    rating: 1,
+    price: 1900,
+    coverSrc:
+      "https://images.unsplash.com/photo-1625244724120-1fd1d34d00f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWxzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 9,
+    title: "Hotel Bliss",
+
+    category: "hotel",
+    cuisine: "village",
+    rating: 4,
+    price: 2750,
+    coverSrc:
+      "https://images.unsplash.com/photo-1629140727571-9b5c6f6267b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aG90ZWxzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 10,
+    title: "Beacon Motel",
+    category: "hotel",
+    cuisine: "urban",
+    rating: 2,
+    price: 4350,
+    coverSrc:
+      "https://images.unsplash.com/photo-1596436889106-be35e843f974?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGhvdGVsc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 11,
+    title: "Holly House",
+    category: "hotel",
+    cuisine: "city",
+    rating: 3,
+    price: 3300,
+    coverSrc:
+      "https://images.unsplash.com/photo-1495365200479-c4ed1d35e1aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGhvdGVsc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 12,
+    title: "Four Seasons",
+    category: "hotel",
+    cuisine: "village",
+    rating: 2,
+    price: 4100,
+    coverSrc:
+      "https://images.unsplash.com/photo-1523699289804-55347c09047d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdGVsc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+];
